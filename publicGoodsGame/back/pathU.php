6b51d4d11424435a0f66a61e2b073b6fa127d8a8
Prajjwal Mishra <prajjwal.mishra99@gmail.com>
2019-07-09 11:46:01 +0530

    removed previous stuff

--- publicGoodsGame/back/pathU.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require ("/home/prajjwalm/PGG/src/botvars.php");
-	require ("/home/prajjwalm/PGG/src/cxnvars.php");
-	require ("/home/prajjwalm/PGG/src/uuid.php");
-	// require_once ("/home/prajjwalm/PGG/src/tblvars.php");
-	
-?>
